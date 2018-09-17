1531656a5e350965a68d1f5cc98ae60997b6464d
chatmichy <mihashi.andapp@gmail.com>
2018-09-17 23:56:00 +0900

    テスト

--- docs/js/app.js
+++ docs/js/app.js
@@ -4,19 +4,22 @@ class App {
 	}
 
 	init() {
+/*
 		var light = new THREE.DirectionalLight(0xFFFFFF);
 		light.position.set(2, 2, 0);
 		scene.add( light );
 
 		var ambientLight = new THREE.AmbientLight(0x888888);
 		scene.add( ambientLight );
-
+*/
+/*
 		this.meshCube = new THREE.Mesh();
 		var geometryCube = new THREE.BoxGeometry(10, 10, 10);
 		var materialCube = new THREE.MeshLambertMaterial( { color: 0x00ff88 } );
 		this.meshCube = new THREE.Mesh( geometryCube, materialCube );
 		this.meshCube.position.set(0, 0, 50);
 		this.scene.add( this.meshCube );
+*/
 
 		var meshSphere = new THREE.Mesh();
 		var loaderSphere = new THREE.TextureLoader();
@@ -30,8 +33,10 @@ class App {
 	}
 
 	update(dt) {
+/*
 		this.meshCube.rotation.x += dt * 0.8
 		this.meshCube.rotation.z += dt * 0.2
+*/
 	}
 
 	render(dt) {
