526a298bf8cbca301e45e88fb34101856ca12d9d
chatmichy <mihashi.andapp@gamil.com>
2019-01-03 17:42:08 +0900

    注視点でイベント発生確認済み

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,4 +1,5 @@
 var boxEl = document.querySelector('a-box');
+var box2El = document.querySelector('#box2');
 
   // カーソルがぶつかったら拡大
   boxEl.addEventListener('mouseenter', function () {
@@ -9,5 +10,17 @@ var boxEl = document.querySelector('a-box');
   // カーソルが離れたら元にもどす
   boxEl.addEventListener('mouseleave', function () {
     boxEl.setAttribute('scale', {x: 2, y: 2, z: 2});
+    console.log("mouseleave");
+  });
+
+  // カーソルがぶつかったら拡大
+  box2El.addEventListener('mouseenter', function () {
+    box2El.setAttribute('scale', {x: 2, y: 2, z: 2});
+    console.log("mouseenter");
+  });
+
+  // カーソルが離れたら元にもどす
+  box2El.addEventListener('mouseleave', function () {
+    box2El.setAttribute('scale', {x: 1, y: 1, z: 1});
     console.log("mouseleave");
   });
